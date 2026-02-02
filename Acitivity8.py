02a0bc87775d8a16590f63173600f2f201d4cd66
goliramitha <142805713+goliramitha@users.noreply.github.com>
2026-02-02 09:53:58 +0530

    Delete Acitivity8.py

--- Acitivity8.py
+++
@@ -1,6 +0,0 @@
-user_input = input("Enter numbers separated by spaces: ")
-numbers = [int(x) for x in user_input.split()]
-if len(numbers) > 0 and numbers[0] == numbers[-1]:
-    print(True)
-else:
-    print(False)
