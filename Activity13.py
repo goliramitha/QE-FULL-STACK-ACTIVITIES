3169e980c39d870b5b6b6fe7d5381d63b86119e5
goliramitha <142805713+goliramitha@users.noreply.github.com>
2026-02-02 09:55:31 +0530

    Delete Activity13.py

--- Activity13.py
+++
@@ -1,8 +0,0 @@
-def my_sum(numbers):
-    total = 0
-    for num in numbers:
-        total += num
-    print("The sum of the list is:", total)
-user_input = input("Enter numbers separated by spaces: ")
-numbers_list = [int(x) for x in user_input.split()]
-my_sum(numbers_list)
