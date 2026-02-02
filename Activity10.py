9d5d28f9bf4d94c3b28e62cb99f404a45070958e
goliramitha <142805713+goliramitha@users.noreply.github.com>
2026-02-02 09:54:48 +0530

    Delete Activity10.py

--- Activity10.py
+++
@@ -1,6 +0,0 @@
-user_input = input("Enter numbers separated by spaces: ")
-numbers = tuple(int(x) for x in user_input.split())
-print("Numbers divisible by 5 are:")
-for num in numbers:
-    if num % 5 == 0:
-        print(num)
