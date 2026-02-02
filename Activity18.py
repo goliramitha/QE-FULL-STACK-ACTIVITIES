e2f454f402f576bb0703dae57f1f0fd8a760a1e1
goliramitha <142805713+goliramitha@users.noreply.github.com>
2026-02-02 09:56:51 +0530

    Delete Activity18.py

--- Activity18.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-df = pd.read_csv("users.csv")
-print(df["Usernames"])
-print(df.loc[1, ["Usernames", "Passwords"]])
-print(df.sort_values("Usernames"))
-print(df.sort_values("Passwords", ascending=False))
